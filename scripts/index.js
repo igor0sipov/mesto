ef192c9228e2be6bf09922688eb1a5fbb9120cc4
Igor Osipov <fakealien@yandex.ru>
2020-08-29 15:39:58 +0300

    form submit ready

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const form = document.querySelector('.form');
 const formNameInput = form.querySelector('.form__name');
 const formBioInput = form.querySelector('.form__bio');
 const formCloseButton = form.querySelector('.form__close-icon');
+const formSubmitButton = form.querySelector('.form__submit-button');
 
 //==========================popup-opening/closing==================================
 
@@ -25,5 +26,15 @@ formCloseButton.addEventListener('click', popupToggle);
 editProfileForm.addEventListener('mousedown', overlayClosing);
 
 //==========================popup-data==================================
+
 formNameInput.value = profileName.innerHTML;
 formBioInput.value = profileBio.innerHTML;
+
+formSubmitHandler = (evt) => {
+  evt.preventDefault();
+  profileName.innerHTML = formNameInput.value;
+  profileBio.innerHTML = formBioInput.value;
+  popupToggle(event);
+}
+
+form.addEventListener('submit', formSubmitHandler);
