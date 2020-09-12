34844236265382b4feed62b0acc9d5d843758f91
Igor Osipov <fakealien@yandex.ru>
2020-09-12 21:42:28 +0300

    feat: pictures loading by js

--- scripts/index.js
+++ scripts/index.js
@@ -42,38 +42,43 @@ form.addEventListener('submit', formSubmitHandler);
 
 const initialCards = [
   {
-    title: 'Дунилово',
-    image: 'https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
-  },
-  {
-    title: 'Хийденсельга',
-    image: 'https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-  },
-  {
-    title: '',
-    image: 'https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80'
-  },
-  {
-    title: 'Кинерма',
-    image: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80'
+    title: 'Алтай',
+    image: 'https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
   },
   {
     title: 'Екатеринбург',
     image: 'https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80'
   },
   {
-    title: 'Алтай',
-    image: 'https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
-  }
+    title: 'Кинерма',
+    image: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80'
+  },
+  {
+    title: 'Калуга',
+    image: 'https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80'
+  },
+  {
+    title: 'Хийденсельга',
+    image: 'https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Дунилово',
+    image: 'https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
+  },
 ];
 
-const cardTemplate = document.querySelector('.card-template');
-const card = document.querySelector('.card');
-const cardTitle = card.querySelector('.card__title');
-const cardImage = card.querySelector('card__image');
-
-function initialCardsRender () {
+function initialCardsRender() {
   initialCards.forEach(card => {
+    const templateContent = document.querySelector('.card-template').content.cloneNode(true);
+    const elements = document.querySelector('.elements');
+    const elementName = templateContent.querySelector('.element__name');
+    const elementPicture = templateContent.querySelector('.element__picture');
+    elementName.textContent = card.title;
+    elementPicture.src = card.image;
+    elements.prepend(templateContent);
+  });
 
-  })
 }
+
+initialCardsRender();
+
