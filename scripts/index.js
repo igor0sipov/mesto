90a8103610ec47f2e30b95d74fc8649d19f34682
Igor Osipov <fakealien@yandex.ru>
2020-09-16 21:53:16 +0300

    refactor: render function

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,34 @@
 //==========================main-variables-init==================================
 
-const editProfileButton = document.querySelector('.profile__edit-button');
-const addPlaceButton = document.querySelector('.profile__add-button')
-const profileName = document.querySelector('.profile__name');
-const profileBio = document.querySelector('.profile__bio');
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addPlaceButton = document.querySelector(".profile__add-button");
+const profileName = document.querySelector(".profile__name");
+const profileBio = document.querySelector(".profile__bio");
 
-const popupPicture = document.querySelector('.popup__picture');
-const popupCaption = document.querySelector('.popup__caption');
+const popupPicture = document.querySelector(".popup__picture");
+const popupCaption = document.querySelector(".popup__caption");
 
-
-
-let addPlacePopup
-let editProfilePopup
-
-const popups = document.querySelectorAll('.popup');
-popups.forEach((elem, index) => {
-  if (popups[index].dataset.whatPopup == 'edit-profile') {
-    editProfilePopup = elem;
-  }
-  if (popups[index].dataset.whatPopup == 'add-place') {
-    addPlacePopup = elem;
-  }
-  if (popups[index].dataset.whatPopup == 'fullsize-photo') {
-    fullsizePhotoPopup = elem;
-  }
-});
-
-let editForm;
-let firstLine;
-let secondLine;
-let closeButton;
-let submitButton;
+const editProfilePopup = document.querySelector(".edit-profile");
+const addPlacePopup = document.querySelector(".add-place");
+const fullsizePhotoPopup = document.querySelector(".fullsize-picture"); // edited
 
 //==========================open/close-popup==================================
 
+let editForm;
 function choosePopup(popupName) {
-  editForm = popupName.querySelector('.popup__container');
-  firstLine = editForm.querySelector('.popup__first-line');
-  secondLine = editForm.querySelector('.popup__second-line');
-  closeButton = editForm.querySelector('.popup__close-icon');
-  submitButton = editForm.querySelector('.popup__submit-button');
+  editForm = popupName.querySelector(".popup__container");
 }
+choosePopup(editProfilePopup);
 
-function toggleClass(name) {
+const firstLine = editForm.querySelector(".popup__first-line");
+const secondLine = editForm.querySelector(".popup__second-line");
+const closeButton = editForm.querySelector(".popup__close-icon");
+const submitButton = editForm.querySelector(".popup__submit-button");
+
+function togglePopup(name) {
   choosePopup(name);
-  name.classList.toggle('popup_opened');
-  closeButton.addEventListener('click', closePopup);
+  name.classList.toggle("popup_opened");
+  closeButton.addEventListener("click", closePopup);
 }
 
 function changePopupContent(popup) {
@@ -55,24 +38,25 @@ function changePopupContent(popup) {
 }
 
 function closePopup(event) {
-
   if (event.target !== event.currentTarget) {
     return;
-  }
-  else if (event.target == closeButton) {
-    toggleClass(event.path[2]);
-  }
-  else if (event.target !== closeButton) {
-    toggleClass(event.target);
+  } else if (event.target == closeButton) {
+    togglePopup(event.path[2]);
+  } else if (event.target !== closeButton) {
+    togglePopup(event.target);
   }
 }
 
-editProfileButton.addEventListener('click', () => toggleClass(editProfilePopup));
-editProfileButton.addEventListener('click', () => changePopupContent(editProfilePopup));
-addPlaceButton.addEventListener('click', () => toggleClass(addPlacePopup));
-editProfilePopup.addEventListener('click', closePopup);
-addPlacePopup.addEventListener('click', closePopup);
-fullsizePhotoPopup.addEventListener('click', closePopup);
+editProfileButton.addEventListener("click", () =>
+  togglePopup(editProfilePopup)
+);
+editProfileButton.addEventListener("click", () =>
+  changePopupContent(editProfilePopup)
+);
+addPlaceButton.addEventListener("click", () => togglePopup(addPlacePopup));
+editProfilePopup.addEventListener("click", closePopup);
+addPlacePopup.addEventListener("click", closePopup);
+fullsizePhotoPopup.addEventListener("click", closePopup);
 
 //==========================editProfilePopup-content==================================
 
@@ -80,85 +64,83 @@ function editProfile(evt) {
   evt.preventDefault();
   profileName.textContent = firstLine.value;
   profileBio.textContent = secondLine.value;
-  toggleClass(editProfilePopup);
+  togglePopup(editProfilePopup);
 }
 
-editProfilePopup.addEventListener('submit', editProfile);
-addPlacePopup.addEventListener('submit', addPlace);
+editProfilePopup.addEventListener("submit", editProfile);
+addPlacePopup.addEventListener("submit", addPlace);
 
 //==========================fullsize-photo-opening==================================
 
 function openPhoto(event) {
   popupPicture.src = event.target.src;
   popupCaption.textContent = event.target.nextElementSibling.textContent;
-  toggleClass(fullsizePhotoPopup);
+  togglePopup(fullsizePhotoPopup);
 }
 
 //==========================template-gallery==================================
 
-let placeCards = [
+const placeCards = [
   {
-    title: 'Алтай',
-    image: 'https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
-    alt: 'Дорога в хвойном лесу'
+    title: "Алтай",
+    image:
+      "https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+    alt: "Дорога в хвойном лесу",
   },
   {
-    title: 'Екатеринбург',
-    image: 'https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80',
-    alt: 'Фото Екатеринбурга с высоты'
+    title: "Екатеринбург",
+    image:
+      "https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    alt: "Фото Екатеринбурга с высоты",
   },
   {
-    title: 'Кинерма',
-    image: 'https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80',
-    alt: 'Два деревянных домика, идет снег'
+    title: "Кинерма",
+    image:
+      "https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80",
+    alt: "Два деревянных домика, идет снег",
   },
   {
-    title: 'Калуга',
-    image: 'https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80',
-    alt: 'Река в лесу окутана туманом'
+    title: "Калуга",
+    image:
+      "https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    alt: "Река в лесу окутана туманом",
   },
   {
-    title: 'Хийденсельга',
-    image: 'https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-    alt: 'Два деревянных домика в снегу у реки на берегу котрой лодка'
+    title: "Хийденсельга",
+    image:
+      "https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: "Два деревянных домика в снегу у реки на берегу котрой лодка",
   },
   {
-    title: 'Дунилово',
-    image: 'https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
-    alt: 'Церковь среди деревьев в поле'
-  }
+    title: "Дунилово",
+    image:
+      "https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    alt: "Церковь среди деревьев в поле",
+  },
 ];
+const elements = document.querySelector(".elements");
+function initializeCard(card) {
+  const template = document.querySelector(".card-template");
+  const templateContent = template.content.cloneNode(true);
+  const elementName = templateContent.querySelector(".element__name");
+  const elementPicture = templateContent.querySelector(".element__picture");
+  const deleteButton = templateContent.querySelector(".element__delete-button");
+  const likeButton = templateContent.querySelector(".element__like-button");
 
-let templateContent;
-let elements;
-let elementName;
-let elementPicture;
-let deleteButton
-let likeButton;
+  elementName.textContent = card.title;
+  elementPicture.src = card.image;
+  elementPicture.alt = card.alt;
 
-function renderPlaceCards() {
-
-  placeCards.forEach(card => {
-    templateContent = document.querySelector('.card-template').content.cloneNode(true);
-    elements = document.querySelector('.elements');
-    elementName = templateContent.querySelector('.element__name');
-    elementPicture = templateContent.querySelector('.element__picture');
-    deleteButton = templateContent.querySelector('.element__delete-button');
-    likeButton = templateContent.querySelector('.element__like-button');
-
-    elementName.textContent = card.title;
-    elementPicture.src = card.image;
-    elementPicture.alt = card.alt;
-    elements.prepend(templateContent);
-    elementPicture.addEventListener('click', openPhoto);
-    deleteButton.addEventListener('click', removePlace);
-    likeButton.addEventListener('click', activateLike)
-  });
-
+  return templateContent;
 }
 
-renderPlaceCards();
+function renderCards () {
+  placeCards.forEach(card => {
+    elements.prepend(initializeCard(card));
+  });
+}
 
+renderCard();
 //==========================adding-new-pics==================================
 
 function addPlace(evt) {
@@ -168,26 +150,25 @@ function addPlace(evt) {
   const newElem = {
     title: firstLine.value,
     image: secondLine.value,
-    alt: firstLine.value
-  }
+    alt: firstLine.value,
+  };
   placeCards.push(newElem);
-  toggleClass(addPlacePopup);
+  togglePopup(addPlacePopup);
   renderPlaceCards();
-  firstLine.value = '';
-  secondLine.value = '';
+  firstLine.value = "";
+  secondLine.value = "";
 }
 
-addPlacePopup.addEventListener('submit', addPlace);
+addPlacePopup.addEventListener("submit", addPlace);
 
 //==========================deleting-pics==================================
 
 function removePlace(event) {
-  event.target.closest('.element').remove();
+  event.target.closest(".element").remove();
 }
-
 
 //==========================like===========================================
 
 function activateLike(event) {
-  event.target.classList.toggle('element__like-button_active');
+  event.target.classList.toggle("element__like-button_active");
 }
