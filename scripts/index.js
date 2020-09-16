cdc06cc7b3ad67f110e8bda920c7fec8844e44a3
Igor Osipov <fakealien@yandex.ru>
2020-09-17 02:20:17 +0300

    refactor: new popup choosing

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-//==========================main-variables-init==================================
+//==========================main-variables-currentPopup==================================
 
 const editProfileButton = document.querySelector(".profile__edit-button");
 const addPlaceButton = document.querySelector(".profile__add-button");
@@ -8,27 +8,13 @@ const profileBio = document.querySelector(".profile__bio");
 const popupPicture = document.querySelector(".popup__picture");
 const popupCaption = document.querySelector(".popup__caption");
 
+const template = document.querySelector(".card-template");
+
 const editProfilePopup = document.querySelector(".edit-profile");
 const addPlacePopup = document.querySelector(".add-place");
 const fullsizePhotoPopup = document.querySelector(".fullsize-picture"); // edited
 
-const template = document.querySelector(".card-template");
 const popups = document.querySelectorAll(".popup");
-
-popups.forEach(el => {
-  const closeButton = el.querySelector('.popup__close-icon');
-  el.addEventListener ('click', (event) => {
-    if (event.target !== event.currentTarget) {
-      return;
-    }
-    togglePopup(el);
-  });
-  closeButton.addEventListener('click', () => {
-    togglePopup(el);
-  });
-});
-
-//==========================open/close-popup==================================
 
 function choosePopup(popupName) {
   const editForm = popupName.querySelector(".popup__container");
@@ -39,16 +25,17 @@ function choosePopup(popupName) {
   return { editForm, firstLine, secondLine, submitButton };
 }
 
+//==========================open/close-popup==================================
+
 function togglePopup(name) {
   name.classList.toggle("popup_opened");
 }
 
 function changePopupContent(popup) {
-  const init = choosePopup(popup);
-  init.firstLine.value = profileName.textContent;
-  init.secondLine.value = profileBio.textContent;
+  const currentPopup = choosePopup(popup);
+  currentPopup.firstLine.value = profileName.textContent;
+  currentPopup.secondLine.value = profileBio.textContent;
 }
-
 
 editProfileButton.addEventListener("click", () =>
   togglePopup(editProfilePopup)
@@ -57,16 +44,28 @@ editProfileButton.addEventListener("click", () =>
   changePopupContent(editProfilePopup)
 );
 addPlaceButton.addEventListener("click", () => togglePopup(addPlacePopup));
-// editProfilePopup.addEventListener("click", closePopup);
-// addPlacePopup.addEventListener("click", closePopup);
-// fullsizePhotoPopup.addEventListener("click", closePopup);
+
+//==========================closing-listeners==================================
+
+popups.forEach((el) => {
+  const closeButton = el.querySelector(".popup__close-icon");
+  closeButton.addEventListener("click", () => togglePopup(el));
+
+  el.addEventListener("click", (event) => {
+    if (event.target !== event.currentTarget) {
+      return;
+    }
+    togglePopup(el);
+  });
+});
 
 //==========================editProfilePopup-content==================================
 
 function editProfile(evt) {
+  const currentPopup = choosePopup(editProfilePopup);
   evt.preventDefault();
-  profileName.textContent = firstLine.value;
-  profileBio.textContent = secondLine.value;
+  profileName.textContent = currentPopup.firstLine.value;
+  profileBio.textContent = currentPopup.secondLine.value;
   togglePopup(editProfilePopup);
 }
 
@@ -86,38 +85,32 @@ function openPhoto(event) {
 const placeCards = [
   {
     title: "Алтай",
-    image:
-      "https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+    image: "https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
     alt: "Дорога в хвойном лесу",
   },
   {
     title: "Екатеринбург",
-    image:
-      "https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    image: "https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
     alt: "Фото Екатеринбурга с высоты",
   },
   {
     title: "Кинерма",
-    image:
-      "https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80",
+    image: "https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80",
     alt: "Два деревянных домика, идет снег",
   },
   {
     title: "Калуга",
-    image:
-      "https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    image: "https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
     alt: "Река в лесу окутана туманом",
   },
   {
     title: "Хийденсельга",
-    image:
-      "https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image: "https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     alt: "Два деревянных домика в снегу у реки на берегу котрой лодка",
   },
   {
     title: "Дунилово",
-    image:
-      "https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    image: "https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
     alt: "Церковь среди деревьев в поле",
   },
 ];
@@ -142,7 +135,7 @@ function initializeCard(card) {
 }
 
 function renderCards() {
-  placeCards.forEach((card) => {
+  placeCards.forEach(card => {
     elements.prepend(initializeCard(card));
   });
 }
@@ -151,15 +144,22 @@ renderCards(); //edited
 
 //==========================adding-new-pics==================================
 
+function clearInputs(input) {
+  input.firstLine.value = "";
+  input.secondLine.value = "";
+}
+
 function addPlace(evt) {
-  choosePopup(addPlacePopup);
+  const currentPopup = choosePopup(addPlacePopup);
   evt.preventDefault();
-  const newElem = {
-    title: firstLine.value,
-    image: secondLine.value,
-    alt: firstLine.value,
+  const newCard = {
+    title: currentPopup.firstLine.value,
+    image: currentPopup.secondLine.value,
+    alt: currentPopup.firstLine.value,
   };
-  elements.prepend(initializeCard(newElem));
+  elements.prepend(initializeCard(newCard));
+  togglePopup(addPlacePopup);
+  clearInputs(currentPopup);
 } //edited
 
 addPlacePopup.addEventListener("submit", addPlace);
