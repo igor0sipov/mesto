cf8be5c18556cee6e09d2e79eecfda23bbde1b2a
Igor Osipov <fakealien@yandex.ru>
2020-09-24 19:26:53 +0300

    feat: start making validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 //==========================main-variables-currentPopup==================================
-
 const editProfileButton = document.querySelector(".profile__edit-button");
 const addPlaceButton = document.querySelector(".profile__add-button");
 const profileName = document.querySelector(".profile__name");
@@ -85,32 +84,38 @@ function openPhoto(event) {
 const placeCards = [
   {
     title: "Алтай",
-    image: "https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+    image:
+      "https://images.unsplash.com/photo-1500101460942-f91854be42e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
     alt: "Дорога в хвойном лесу",
   },
   {
     title: "Екатеринбург",
-    image: "https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    image:
+      "https://images.unsplash.com/photo-1526722021192-1c0dc9b0921d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
     alt: "Фото Екатеринбурга с высоты",
   },
   {
     title: "Кинерма",
-    image: "https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80",
+    image:
+      "https://images.unsplash.com/photo-1559029884-4e34093db5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=1349&q=80",
     alt: "Два деревянных домика, идет снег",
   },
   {
     title: "Калуга",
-    image: "https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+    image:
+      "https://images.unsplash.com/photo-1505551071487-d4a3fd384857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
     alt: "Река в лесу окутана туманом",
   },
   {
     title: "Хийденсельга",
-    image: "https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image:
+      "https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     alt: "Два деревянных домика в снегу у реки на берегу котрой лодка",
   },
   {
     title: "Дунилово",
-    image: "https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    image:
+      "https://images.unsplash.com/photo-1570579425144-46bcf064db84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
     alt: "Церковь среди деревьев в поле",
   },
 ];
@@ -135,7 +140,7 @@ function initializeCard(card) {
 }
 
 function renderCards() {
-  placeCards.forEach(card => {
+  placeCards.forEach((card) => {
     elements.prepend(initializeCard(card));
   });
 }
@@ -174,4 +179,8 @@ function removePlace(event) {
 
 function activateLike(event) {
   event.target.classList.toggle("element__like-button_active");
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
 }
