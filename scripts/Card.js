a374711896d768fa6a139e7bc74ba5782a9495c9
Igor Osipov <fakealien@yandex.ru>
2020-10-10 16:37:52 +0300

    reafactor: card class created

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,5 +1,63 @@
 class Card {
-  constructor () {
+  constructor(data, cardSelector) {
+    this._title = data.title;
+    this._image = data.image;
+    this._alt = data.alt;
+    this._cardSelector = cardSelector;
+  }
 
+  _togglePopup() {
+    document.querySelector('.fullsize-pictre').classList.toggle("popup_opened");
+  }
+
+
+  _openFullsizePhoto(event) {
+    document.querySelector('.popup__picture').src = event.target.src;
+    document.querySelector('.popup__caption').textContent = event.target.nextElementSibling.textContent;
+    this._togglePopup();
+  }
+
+  _removePlace(event) {
+    event.target.closest('.element').remove();
+  }
+
+  _activateLike(event) {
+    event.target.classList.toggle('element__like-button_active')
+  }
+
+  _getTemplate() {
+    const templateContent = document
+      .querySelector(this._cardSelector)
+      .content.cloneNode(true);
+
+      return templateContent;
+  }
+
+  _setListeners() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector('.element__name').addEventListener('click', evt => {
+      this._openFullsizePhoto(evt);
+    })
+    this._element.querySelector('.element__delete-button').addEventListener('click', evt => {
+      this._removePlace(evt);
+    })
+    this._element.querySelector('.element__like-button').addEventListener('click', evt => {
+      this._activateLike(evt);
+    } )
+  }
+
+  initializeCard() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector('.element__name').textContent = this._title;
+    this._element.querySelector('.element__picture').src = this._image;
+    this._element.querySelector('.element__picture').alt = this._alt;
+
+    this._setListeners();
+
+    return this._element;
   }
 }
+
+export default Card;
